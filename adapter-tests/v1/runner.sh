7536643bbec4d98bd658ccb4040af8383082f0bf
ci <ci@example.com>
2026-08-31 15:49:11 -0400

    Silence signal cleanup notices

--- adapter-tests/v1/runner.sh
+++ adapter-tests/v1/runner.sh
@@ -60,6 +60,7 @@ signal_exit() {
   local status=$1
   trap '' HUP INT TERM
   trap - EXIT
+  exec >/dev/null 2>&1
   terminate_active_group
   cleanup
   exit "$status"
--- scripts/test/portable-adapter-contracts.test.sh
+++ scripts/test/portable-adapter-contracts.test.sh
@@ -403,10 +403,33 @@ done
 signal_status=0
 wait "$signal_pid" || signal_status=$?
 /bin/sleep 2.2
-[ "$signal_sent" -gt 0 ] && [ "$signal_status" -eq 143 ] &&
-  [ ! -s "$signal_out" ] && [ ! -s "$signal_err" ] &&
-  [ -z "$(find "$fixture/scratch" -mindepth 1 -print -quit)" ] ||
+signal_stdout_bytes=$(/usr/bin/wc -c < "$signal_out" | /usr/bin/tr -d ' ')
+signal_stderr_bytes=$(/usr/bin/wc -c < "$signal_err" | /usr/bin/tr -d ' ')
+signal_stdout_class=empty
+signal_stderr_class=empty
+[ "$signal_stdout_bytes" -eq 0 ] || signal_stdout_class=other
+if [ "$signal_stderr_bytes" -ne 0 ]; then
+  if /usr/bin/grep -Eq '(^|[[:space:]])(Terminated|Killed|Hangup|Interrupt|Done)(:|[[:space:]]|$)' \
+      "$signal_err"; then
+    signal_stderr_class=job-control
+  else
+    signal_stderr_class=other
+  fi
+fi
+signal_scratch_count=$(/usr/bin/find "$fixture/scratch" -mindepth 1 -print | /usr/bin/wc -l | /usr/bin/tr -d ' ')
+signal_survivor=absent
+if /usr/bin/find "$fixture/scratch" \( -name timeout-survived -o -name descendant-survived \) \
+    -print -quit | /usr/bin/grep -q .; then
+  signal_survivor=present
+fi
+if [ "$signal_sent" -le 0 ] || [ "$signal_status" -ne 143 ] ||
+   [ "$signal_stdout_bytes" -ne 0 ] || [ "$signal_stderr_bytes" -ne 0 ] ||
+   [ "$signal_scratch_count" -ne 0 ] || [ "$signal_survivor" != absent ]; then
+  /usr/bin/printf 'signal-debug status=%s sent=%s stdout_bytes=%s stdout_class=%s stderr_bytes=%s stderr_class=%s scratch_entries=%s survivor=%s\n' \
+    "$signal_status" "$signal_sent" "$signal_stdout_bytes" "$signal_stdout_class" \
+    "$signal_stderr_bytes" "$signal_stderr_class" "$signal_scratch_count" "$signal_survivor" >&2
   fail 'signal group cleanup'
+fi
 pass 'TERM stops adapter group and cleans scratch'
 
 source_saved="$tmp/source.saved"
